6d353beff17db5b239deb2b69e12be12a8206bf0
Binh Minh <bmmit.work@gmail.com>
2025-04-16 22:39:05 +0700

    feat: shadcn, tailwind, landingpage

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,25 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/image']
+  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/image'],
+  typescript: {
+    typeCheck: true
+  },
+  css: ['~/assets/css/tailwind.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  imports: {
+    autoImport: false
+  },
+  eslint: {
+    config: {
+      standalone: false
+    }
+  }
 })
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { H1, H3 } from '@/components/ui/typography'
+import { ArrowRight } from 'lucide-vue-next'
+</script>
+
+<template>
+  <div class="flex min-h-screen flex-col items-center justify-center gap-4 p-4 text-center">
+    <NuxtImg src="/logo.svg" class="size-24 mb-4" />
+
+    <H1 class="text-primary font-mono">
+      PTNK Library
+    </H1>
+    <H3>A simple, sleek and reliable library management service</H3>
+    
+    <Button size="lg" class="mt-2 shadow-xl">
+      Getting started <ArrowRight />
+    </Button>
+  </div>
+</template>
